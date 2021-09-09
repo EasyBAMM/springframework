40bc9f0cf9ac8f2707f32477da5ee929e281352a
EasyBAMM <huhjb1020@naver.com>
2021-09-09 09:02:37 +0900

    add DI

--- src/main/java/com/mycompany/webapp/service/Ch13Service1.java
+++ src/main/java/com/mycompany/webapp/service/Ch13Service1.java
@@ -3,11 +3,32 @@ package com.mycompany.webapp.service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class Ch13Service1 {
+import com.mycompany.webapp.dao.Ch13BoardDao1;
 
+public class Ch13Service1 {
+	/* XML을 이용한 의존성 주입 */
 	private static final Logger logger = LoggerFactory.getLogger(Ch13Service1.class);
+
+	private Ch13BoardDao1 ch13BoardDao1;
 
 	public Ch13Service1() {
 		logger.info("실행");
+	}
+
+	// 의존성 주입을 위한 생성자 선언
+	public Ch13Service1(Ch13BoardDao1 ch13BoardDao1) {
+		logger.info("Ch13Service1(Ch13BoardDao1 ch13BoardDao1) 실행");
+		this.ch13BoardDao1 = ch13BoardDao1;
+	}
+
+	// 의존성 주입을 위한 Setter 선언
+	public void setCh13BoardDao1(Ch13BoardDao1 ch13BoardDao1) {
+		logger.info("실행");
+		this.ch13BoardDao1 = ch13BoardDao1;
+	}
+
+	public void method1() {
+		logger.info("실행");
+		ch13BoardDao1.update(); // NullPointerException
 	}
 }
