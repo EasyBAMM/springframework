e7a0c82ccf5c74f71dba819ea0acc25f4bcc8926
EasyBAMM <huhjb1020@naver.com>
2021-09-16 18:01:34 +0900

    add transaction, security

--- src/main/java/com/mycompany/webapp/service/Ch16AccountService.java
+++ src/main/java/com/mycompany/webapp/service/Ch16AccountService.java
@@ -8,15 +8,21 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.support.TransactionCallback;
 import org.springframework.transaction.support.TransactionTemplate;
 
 import com.mycompany.webapp.dao.Ch16AccountDao;
 import com.mycompany.webapp.dto.Ch16Account;
+import com.mycompany.webapp.exception.Ch16NotFoundAccountException;
 
 @Service
 public class Ch16AccountService {
 	private static final Logger logger = LoggerFactory.getLogger(Ch16AccountService.class);
+
+	public enum TransferResult {
+		SUCCESS, FAIL_NOT_FOUND_ACCOUNT, FAIL_NOT_ENOUGH_BALANCE,
+	}
 
 	// 트랜잭션 주입
 	@Resource
@@ -31,41 +37,57 @@ public class Ch16AccountService {
 		return accounts;
 	}
 
-	// 트랜잭션 처리가 되어있다.
-	public void transfer1(int fromAno, int toAno, int amount) {
+	// 프로그래밍 방식 - 컨트롤러에서 에러 처리 시
+	public TransferResult transfer1(int fromAno, int toAno, int amount) {
 		logger.info("실행");
 
 		// 트랜잭션 처리 Callback
 		String result = transactionTemplate.execute(new TransactionCallback<String>() {
 			@Override
 			public String doInTransaction(TransactionStatus status) {
-				return null;
+				try {
+					// 출금하기
+					Ch16Account fromAccount = accountDao.selectByAno(fromAno);
+					fromAccount.setBalance(fromAccount.getBalance() - amount);
+					accountDao.updateBalance(fromAccount);
+
+					// 예금하기
+					Ch16Account toAccount = accountDao.selectByAno(toAno);
+					toAccount.setBalance(toAccount.getBalance() + amount);
+					accountDao.updateBalance(toAccount);
+					return "success";
+				} catch (Exception e) {
+					// 트랜잭션 작업을 모두 취소
+					status.setRollbackOnly();
+					return "fail";
+				}
 			}
 		});
 
-		// 출금하기
-		Ch16Account fromAccount = accountDao.selectByAno(fromAno);
-		fromAccount.setBalance(fromAccount.getBalance() - amount);
-		accountDao.updateBalance(fromAccount);
-
-		// 예금하기
-		Ch16Account toAccount = accountDao.selectByAno(toAno);
-		toAccount.setBalance(toAccount.getBalance() + amount);
-		accountDao.updateBalance(toAccount);
+		if (result.equals("success")) {
+			return TransferResult.SUCCESS;
+		} else {
+			return TransferResult.FAIL_NOT_FOUND_ACCOUNT;
+		}
 	}
 
-	// 트랜잭션 처리가 전혀 되어있지 않다.
+	// 선언적 방식 - exception으로 에러처리
+	@Transactional
 	public void transfer2(int fromAno, int toAno, int amount) {
 		logger.info("실행");
 
-		// 출금하기
-		Ch16Account fromAccount = accountDao.selectByAno(fromAno);
-		fromAccount.setBalance(fromAccount.getBalance() - amount);
-		accountDao.updateBalance(fromAccount);
+		try {
+			// 출금하기
+			Ch16Account fromAccount = accountDao.selectByAno(fromAno);
+			fromAccount.setBalance(fromAccount.getBalance() - amount);
+			accountDao.updateBalance(fromAccount);
 
-		// 예금하기
-		Ch16Account toAccount = accountDao.selectByAno(toAno);
-		toAccount.setBalance(toAccount.getBalance() + amount);
-		accountDao.updateBalance(toAccount);
+			// 예금하기
+			Ch16Account toAccount = accountDao.selectByAno(toAno);
+			toAccount.setBalance(toAccount.getBalance() + amount);
+			accountDao.updateBalance(toAccount);
+		} catch (Exception e) {
+			throw new Ch16NotFoundAccountException("계좌가 존재하지 않습니다.");
+		}
 	}
 }
